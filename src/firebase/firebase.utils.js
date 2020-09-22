63f1258e9d213ec88d5cd628a973e54e10172676
Chandan Jagdish Panjwani <cpanjwani0000@gmail.com>
2020-09-23 03:48:31 +0530

    finished sign-in and just installed firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCjDlMeX6IrSiVBel2-PzG2oCmused2EKg",
+    authDomain: "crown-db-b2051.firebaseapp.com",
+    databaseURL: "https://crown-db-b2051.firebaseio.com",
+    projectId: "crown-db-b2051",
+    storageBucket: "crown-db-b2051.appspot.com",
+    messagingSenderId: "833735036937",
+    appId: "1:833735036937:web:739ff24936e73b5d4fe8d8",
+    measurementId: "G-38RERL6Q4P"
+};
+
+firebase.initializeApp(config);
