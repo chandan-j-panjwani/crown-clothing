59057544e58b4f57502170ecf1b78086ba9c6d93
Chandan Jagdish Panjwani <cpanjwani0000@gmail.com>
2020-09-24 01:54:52 +0530

    styled sign in and enabled signin firebase google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,25 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyCjDlMeX6IrSiVBel2-PzG2oCmused2EKg",
-    authDomain: "crown-db-b2051.firebaseapp.com",
-    databaseURL: "https://crown-db-b2051.firebaseio.com",
-    projectId: "crown-db-b2051",
-    storageBucket: "crown-db-b2051.appspot.com",
-    messagingSenderId: "833735036937",
-    appId: "1:833735036937:web:739ff24936e73b5d4fe8d8",
-    measurementId: "G-38RERL6Q4P"
+  apiKey: "AIzaSyCjDlMeX6IrSiVBel2-PzG2oCmused2EKg",
+  authDomain: "crown-db-b2051.firebaseapp.com",
+  databaseURL: "https://crown-db-b2051.firebaseio.com",
+  projectId: "crown-db-b2051",
+  storageBucket: "crown-db-b2051.appspot.com",
+  messagingSenderId: "833735036937",
+  appId: "1:833735036937:web:739ff24936e73b5d4fe8d8",
+  measurementId: "G-38RERL6Q4P",
 };
 
 firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
