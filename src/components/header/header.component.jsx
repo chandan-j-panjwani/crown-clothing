97f3f899ddd07b65d03e6d7a6cf5df9d9a617aca
Chandan Jagdish Panjwani <cpanjwani0000@gmail.com>
2020-10-06 21:13:01 +0530

    installed styled-components library, converted styles of header and homepage

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,33 +9,34 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {selectCartHidden} from '../../redux/cart/cart.selectors.js';
 import {selectCurrentUser} from '../../redux/user/user.selector.js';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import "./header.styles.scss";
+
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink} from './header.styles';
 
 const Header = ({ currentUser,hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
+  <HeaderContainer>
+    <LogoContainer to='/'>
       <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>
         SHOP
-      </Link>
-      <Link className='option' to='/shop'>
+      </OptionLink>
+      <OptionLink to='/shop'>
         CONTACT
-      </Link>
+      </OptionLink>
       {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
+        <OptionDiv onClick={() => auth.signOut()}>
           SIGN OUT
-        </div>
+        </OptionDiv>
       ) : (
-        <Link className='option' to='/signin'>
+        <OptionLink to='/signin'>
           SIGN IN
-        </Link>
+        </OptionLink>
       )}
       <CartIcon />
-    </div>
+    </OptionsContainer>
     { hidden ? null : <CartDropdown /> }
-  </div>
+    </HeaderContainer>
 
 );
 
